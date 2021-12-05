eb5767fcd41c2d1fd22871b1b0f782b145b0c364
Elen <elen.ghazaryan6@gmail.com>
2021-12-05 16:23:56 +0400

    feat: write with 'While' and 'Do While' loops

---
+++ whileDoWhile.js
@@ -0,0 +1,14 @@
+let a = [1, 2, 3];
+let i = 0;
+
+while (i < a.length) {
+  console.log("for", i, a[i]);
+  i++;
+}
+
+let a = [1, 2, 3];
+let i = 0;
+do {
+  console.log("for", i, a[i]);
+  i++;
+} while (i < a.length);
