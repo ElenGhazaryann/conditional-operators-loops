198fb5a273caabec05df9e6680a14b81069265ac
Elen <elen.ghazaryan6@gmail.com>
2021-12-05 17:28:34 +0400

    feat: arrange odd numbers first, then even numbers

---
+++ t13_arrangeOddsEvens.js
@@ -0,0 +1,12 @@
+function my_f(arr) {
+  let even = [];
+  let odd = [];
+  arr.filter(function (val) {
+    if (typeof val == "number") {
+      return val % 2 === 0 ? even.push(val) : odd.push(val);
+    }
+  });
+
+  return odd.concat(even);
+}
+console.log(my_f([8, 8, "meh", 6]));
