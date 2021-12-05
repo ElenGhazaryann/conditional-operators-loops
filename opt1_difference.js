5fc25c116fbc01853162d50f2f289c7f8e546830
Elen <elen.ghazaryan6@gmail.com>
2021-12-05 18:18:46 +0400

    feat: find difference between max and min digit

---
+++ opt1_difference.js
@@ -0,0 +1,7 @@
+function my_f(num) {
+  let arr = num.toString().split("");
+  let maxNum = Math.max(...arr);
+  let minNum = Math.min(...arr);
+  return maxNum - minNum;
+}
+console.log(my_f(4593653));
