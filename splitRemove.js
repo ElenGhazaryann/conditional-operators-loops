4262b3077958aa91b3a844e9e6838c2b4d9b2da1
Elen <elen.ghazaryan6@gmail.com>
2021-12-05 17:45:07 +0400

    feat: split sentence and remove last . or !

---
+++ splitRemove.js
@@ -0,0 +1,14 @@
+function my_f(sent) {
+  let arr = sent.split(" ");
+  let newArr = [];
+  let lastArr = [];
+  for (let i = arr.length - 1; i < arr.length; i++) {
+    newArr = arr[i].split("");
+    newArr.pop();
+    lastArr = newArr.join("");
+  }
+
+  arr.pop();
+  return arr.concat(lastArr);
+}
+console.log(my_f("May the Force be with you."));
