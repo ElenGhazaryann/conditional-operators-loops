1f06aa6a10e688ba90cc8c47bb7e006d243a632d
Elen <elen.ghazaryan6@gmail.com>
2021-12-05 17:34:09 +0400

    feat: find number of strings and numbers in an array

---
+++ t14_numberOfNumStr.js
@@ -0,0 +1,10 @@
+function my_f(arr) {
+  let num = [];
+  let str = [];
+  for (let i = 0; i < arr.length; i++) {
+    typeof arr[i] === "number" ? num.push(arr[i]) : str.push(arr[i]);
+  }
+  return `Numbers : ${num.length}, Strings : ${str.length}`;
+}
+
+console.log(my_f([1, 4, "i am a string", "456"]));
