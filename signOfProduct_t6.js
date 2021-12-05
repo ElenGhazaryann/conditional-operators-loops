b1715b05d358681cf39843b6f155f68c344bed1f
Elen <elen.ghazaryan6@gmail.com>
2021-12-05 16:48:06 +0400

    feat: write sign of product of 3numbers

---
+++ signOfProduct_t6.js
@@ -0,0 +1,16 @@
+function signOfProduct(a, b, c) {
+  if (a > 0 && b > 0 && c > 0) {
+    return "Sign is +";
+  } else if (a > 0 && b < 0 && c < 0) {
+    return "Sign is +";
+  } else if (a < 0 && b > 0 && c < 0) {
+    return "Sign is +";
+  } else if (a < 0 && b < 0 && c > 0) {
+    return "Sign is +";
+  } else if (a === 0 || b === 0 || c === 0) {
+    return "Unsigned";
+  } else {
+    return "Sign is -";
+  }
+}
+console.log(signOfProduct(4, 19, -2));
